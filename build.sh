c5529328f293ba23fb6817bf35da98d009175a03
Nicolas Couture <nicolas.couture@gmail.com>
2020-12-27 22:55:13 -0500

    removing unused stuff from build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #
 
-export _PYTHON=$(python -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')
-export VIRTUAL_ENV
-
 bazel build firestore_middleware:app
